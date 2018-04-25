0cf42894a1f67ff7ea860c1b6fd5d7ada10c44c0
Oghlukyan <oghlukyan@gmail.com>
2018-04-25 05:41:56 +0400

    advanced fixed

--- script.js
+++ script.js
@@ -43,24 +43,14 @@ function makeTable() {
         field += '<div id="' + i + '" class="row">';
         for (var j = 0; j < size; j++){
             field += '<div class="field border">\n' +
-                     '    <div id="' + i + '.' + j + '" class="content" onclick="fieldClickListener(this)">\n' +
-                     '    </div>\n' +
-                     '</div>';
+                '    <div id="' + i + '.' + j + '" class="content" onclick="fieldClickListener(this)">\n' +
+                '    </div>\n' +
+                '</div>';
             table[i].push('');
         }
         field += '</div>';
     }
     $('#table_container').append(field);
-}
-
-function getDirection(id) {
-    var currentDirection = [0, 0];
-    var i = 0;
-    while (id[i] !== '.')
-        currentDirection[0] = currentDirection[0]*10 + parseInt(id[i++]);
-    while (++i < id.length)
-        currentDirection[1] = currentDirection[1]*10 + parseInt(id[i]);
-    return currentDirection;
 }
 
 function fieldClickListener(element) {
@@ -80,116 +70,107 @@ function randomField() {
     fill(currentDirection[0], currentDirection[1]);
 }
 
-function fill(row, column) {
-    var current = queue ? "X" : "O";
-    table[row][column] = current;
-    $('#' + row + '\\.' + column).html(current).css('font-size', 750/size);
-    queue = !queue;
-    myQueue = !myQueue;
-    blockFilled(row, column);
+function getDirection(id) {
+    var currentDirection = [0, 0];
+    var i = 0;
+    while (id[i] !== '.')
+        currentDirection[0] = currentDirection[0]*10 + parseInt(id[i++]);
+    while (++i < id.length)
+        currentDirection[1] = currentDirection[1]*10 + parseInt(id[i]);
+    return currentDirection;
 }
 
-function blockFilled(row, column) {
+function fill(row, column) {
+    var current = queue ? 'X' : 'O';
+    table[row][column] = current;
+    $('#' + row + '\\.' + column).html(current).css('font-size', 750/size);
     var finish = checkIfFinished(row, column);
     if (finish === 'W') {
         $('#status').html(finish).css('color', '#0F0');
-    } else if (finish === 'L'){
+    } else if (finish === 'L') {
         $('#status').html(finish).css('color', '#F00');
-    } else if (finish === 'D'){
+    } else if (finish === 'D') {
         $('#status').html(finish).css('color', '#BBB');
-    } else {
-        if (!myQueue) {
-            randomField();
-        }
     }
+    queue = !queue;
+    myQueue = !myQueue;
+    if (!myQueue && !finished)
+        randomField();
 }
 
 function checkIfFinished(row, column) {
-    var color = myQueue ? '#F00' : '#0F0';
-    var status = myQueue ? 'L' : 'W';
+    var color = myQueue ? '#0F0' : '#F00';
+    var status = myQueue ? 'W' : 'L';
+    var current = queue ? 'X' : 'O';
     var check;
-    var a, b;
-    // diagonal
-    for (var i = -2; i <= 0; i++) {
-        if (row+i < 0 || row+i+2 >= size || column+i < 0 || column+i+2 >= size)
-            continue;
-        check = 0;
-        for (var j = 0; j <= 2; j++)
-            if (table[row][column] === table[row+i+j][column+i+j])
-                check++;
-        if (check === 3) {
-            a = 0;
-            b = 0;
-            for (j = 0; j <= 2; j++) {
-                a = row+i+j;
-                b = column+i+j;
-                $('#' + a + '\\.' + b).css('color', color);
+    // lines
+    // horizontal line
+    check = true;
+    for (var i = 0; i < size; i++)
+        if (table[row][i] !== current)
+            check = false;
+    if (check) {
+        for (i = 0; i < size; i++)
+            $('#' + row + '\\.' + i).css('color', color);
+        finished = true;
+        return status;
+    }
+    // vertical line
+    check = true;
+    for (i = 0; i < size; i++)
+        if (table[i][column] !== current)
+            check = false;
+    if (check) {
+        for (i = 0; i < size; i++)
+            $('#' + i + '\\.' + column).css('color', color);
+        finished = true;
+        return status;
+    }
+    // diagonals
+    var left, right;
+    var currentRow, currentColumn;
+    // main diagonal
+    if (!(row === 0 && column === size-1)  && !(row === size-1 && column === 0)) {
+        check = true;
+        left = (row < column) ? (row) : (column);
+        right = (size - row < size - column) ? (size - row) : (size - column);
+        for (i = -left; i < right; i++)
+            if (table[row + i][column + i] !== current)
+                check = false;
+        if (check) {
+            for (i = -left; i < right; i++) {
+                currentRow = row + i;
+                currentColumn = column + i;
+                $('#' + currentRow + '\\.' + currentColumn).css('color', color);
             }
             finished = true;
             return status;
         }
     }
     // opposite diagonal
-    for (i = -2; i <= 0; i++) {
-        if (row+i < 0 || row-i+2 >= size || column+i-2 < 0 || column+i >= size)
-            continue;
-        check = 0;
-        for (j = 0; j <= 2; j++)
-            if (table[row][column] === table[row+i+j][column-i-j])
-                check++;
-        if (check === 3) {
-            a = 0;
-            b = 0;
-            for (j = 0; j <= 2; j++) {
-                a = row+i+j;
-                b = column-i-j;
-                $('#' + a + '\\.' + b).css('color', color);
+    if (!(row === 0 && column === 0) && !(row === size-1 && column === size-1)) {
+        check = true;
+        left = (size-1 - row < column) ? (size-1 - row) : (column);
+        right = (row+1 < size - column) ? (row+1) : (size - column);
+        for (i = -left; i < right; i++)
+            if (table[row - i][column + i] !== current)
+                check = false;
+        if (check) {
+            for (i = -left; i < right; i++) {
+                currentRow = row - i;
+                currentColumn = column + i;
+                $('#' + currentRow + '\\.' + currentColumn).css('color', color);
             }
             finished = true;
             return status;
         }
     }
-    // horizontal
-    for (i = -2; i <= 0; i++) {
-        if (column+i < 0 || column+i+2 >= size)
-            continue;
-        check = 0;
-        for (j = 0; j <= 2; j++)
-            if (table[row][column] === table[row][column+i+j])
-                check++;
-        if (check === 3) {
-            b = 0;
-            for (j = 0; j <= 2; j++) {
-                b = column+i+j;
-                $('#' + row + '\\.' + b).css('color', color);
-            }
-            finished = true;
-            return status;
-        }
-    }
-    // vertical
-    for (i = -2; i <= 0; i++) {
-        if (row+i < 0 || row+i+2 >= size)
-            continue;
-        check = 0;
-        for (j = 0; j <= 2; j++)
-            if (table[row][column] === table[row+i+j][column])
-                check++;
-        if (check === 3) {
-            a = 0;
-            for (j = 0; j <= 2; j++) {
-                a = row+i+j;
-                $('#' + a + '\\.' + column).css('color', color);
-            }
-            finished = true;
-            return status;
-        }
-    }
+    check = true;
     for (i = 0; i < size; i++)
         for (j = 0; j < size; j++)
             if (!table[i][j])
-                return false;
-    return 'D';
+                check = false;
+    return check ? 'D' : false;
 }
 
 function replay() {
